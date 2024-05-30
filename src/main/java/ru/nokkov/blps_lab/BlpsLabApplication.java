146a7ef1d497b26596ab878ac5ef4f5f1dbdf3c8
Arkady Chesnokov <arkady-chesnokof@yandex.ru>
2024-05-30 17:41:11 +0300

    init commit

---
+++ src/main/java/ru/nokkov/blps_lab/BlpsLabApplication.java
@@ -0,0 +1,12 @@
+package ru.nokkov.blps_lab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlpsLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlpsLabApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/nokkov/blps_lab/BlpsLabApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nokkov.blps_lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlpsLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
