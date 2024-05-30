3f5f909697020b44f62158d1acba92c317bae30e
Arkady Chesnokov <arkady-chesnokof@yandex.ru>
2024-05-30 18:07:02 +0300

    configure 2 datasources

---
+++ src/main/java/ru/nokkov/blps_lab/config/ArticleDatasourceConfig.java
@@ -0,0 +1,63 @@
+package ru.nokkov.blps_lab.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import jakarta.persistence.EntityManagerFactory;
+
+@Configuration
+@EnableTransactionManagement
+
+@EnableJpaRepositories(
+        basePackages = "ru.nokkov.blps_lab.article",
+        entityManagerFactoryRef = "articleEntityManagerFactory",
+        transactionManagerRef = "partnerTxManager"
+)
+public class ArticleDatasourceConfig {
+
+    @Bean
+    @ConfigurationProperties("spring.article.datasource")
+    public DataSourceProperties articleDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    @Primary
+    public DataSource articleDataSource() {
+        return articleDataSourceProperties()
+                .initializeDataSourceBuilder()
+                .build();
+    }
+
+    @Bean
+    @Primary
+    public LocalContainerEntityManagerFactoryBean articleEntityManagerFactory (
+            @Qualifier("articleDataSource") DataSource dataSource,
+            EntityManagerFactoryBuilder builder
+    ) {
+        return builder
+                .dataSource(dataSource)
+                .packages("ru.nokkov.blps_lab.article")
+                .persistenceUnit("articlePersistenceUnit")
+                .build();
+    }
+
+    @Bean(name = "articleTxManager")
+    @Primary
+    public PlatformTransactionManager transactionManager(
+            @Qualifier("articleEntityManagerFactory") EntityManagerFactory entityManagerFactory) {
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+}
---
+++ src/main/java/ru/nokkov/blps_lab/config/PartnerDatasourceConfig.java
@@ -0,0 +1,61 @@
+package ru.nokkov.blps_lab.config;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.orm.jpa.EntityManagerFactoryBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import jakarta.persistence.EntityManagerFactory;
+
+@Configuration
+@EnableTransactionManagement
+
+@EnableJpaRepositories(
+        basePackages = "ru.nokkov.blps_lab.partner",
+        entityManagerFactoryRef = "partnerEntityManagerFactory",
+        transactionManagerRef = "partnerTxManager"
+)
+public class PartnerDatasourceConfig {
+
+    @Bean
+    @ConfigurationProperties("spring.partner.datasource")
+    public DataSourceProperties partnerDataSourceProperties() {
+        return new DataSourceProperties();
+    }
+
+    @Bean
+    public DataSource partnerDataSource() {
+        return partnerDataSourceProperties()
+                .initializeDataSourceBuilder()
+                .build();
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean partnerEntityManagerFactory (
+            @Qualifier("partnerDataSource") DataSource dataSource,
+            EntityManagerFactoryBuilder builder
+    ) {
+        return builder
+                .dataSource(dataSource)
+                .packages("ru.nokkov.blps_lab.partner")
+                .persistenceUnit("partner")
+                .build();
+    }
+
+    @Bean(name = "partnerTxManager")
+    public PlatformTransactionManager transactionManager(
+            @Qualifier("partnerEntityManagerFactory") EntityManagerFactory entityManagerFactory) {
+
+        return new JpaTransactionManager(entityManagerFactory);
+    }
+}
